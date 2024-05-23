a1c4a249d1a1de4ba58d05d2038c5121a5cb567a
js <59955248+ysj13@users.noreply.github.com>
2024-05-23 09:34:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        
+        if(n == 1) {
+            return 0;
+        }
+        
+        while(n > 1) {
+            if(n % 2 == 0) {
+                n = n / 2;
+                answer++;
+            } else {
+                n = (n * 3) + 1;
+                answer++;
+            }
+            
+            if(answer >= 500) {
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
