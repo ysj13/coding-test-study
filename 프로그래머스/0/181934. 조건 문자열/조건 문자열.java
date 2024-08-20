af857580b3293564c733704b501c618c27ff5e33
js <59955248+ysj13@users.noreply.github.com>
2024-08-20 09:45:32 +0900

    [level 0] Title: 조건 문자열, Time: 2.52 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+
+        return switch(ineq + eq) {
+            case ">=" -> (n >= m) ? 1 : 0;
+            case "<=" -> (n <= m) ? 1 : 0;
+            case ">!" -> (n > m) ? 1 : 0;
+            case "<!" -> (n < m) ? 1 : 0;
+            default -> 0;
+        };
+    }
+}
