e3e341b11c07b5c9070193402895cda63feb5366
js <59955248+ysj13@users.noreply.github.com>
2024-08-20 09:24:23 +0900

    [level 0] Title: 더 크게 합치기, Time: 13.80 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(a + "" + b);
+        int ba = Integer.parseInt(b + "" + a);
+        
+        return Math.max(ab, ba);
+    }
+}
