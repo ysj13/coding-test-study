0eec05f0c12d782000f5e542ed899b01bdf7e263
js <59955248+ysj13@users.noreply.github.com>
2024-12-16 11:36:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 916 ms, Memory: 98936 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    final static int MAX = 1000000 + 10;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M, R;
+    static int[] answer;
+    static int order;
+
+    public static void dfs(int n) {
+        visited[n] = true;
+        answer[n] = order;
+        order++;
+
+        for(int i = 0; i < graph[n].size(); i++) {
+            int next = graph[n].get(i);
+            
+            if (visited[next] == false) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        
+        graph = new ArrayList[MAX];
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        visited = new boolean[MAX];
+        answer = new int[MAX];
+        order = 1;
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[y].add(x);
+            graph[x].add(y);
+        }
+
+        for(int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+            Collections.reverse(graph[i]);
+        }
+
+        dfs(R);
+
+        for(int i = 1; i <=N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+        
+        bw.close();
+        br.close();
+    }
+}
